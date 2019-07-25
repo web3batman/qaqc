e54ff3b0aae502f406b7d9c08af85434e1d9b164
edyadan <edyadan@outlook.com>
2019-07-25 11:58:31 -0500

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, jsonify, request
-from sklearn.externals import joblib
-import sklearn
+from flask import Flask, jsonify, request, render_template, redirect, url_for, json
+from flask import send_file
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.attributes import flag_modified
+import pandas as pd
+
+import os
+from glob import glob
+
+from docxtpl import DocxTemplate, InlineImage
+from docx.shared import Mm, Inches, Pt
+import io
+import barcode
+from barcode.writer import ImageWriter
 import cv2
+
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+from apiclient.http import MediaIoBaseDownload
+from apiclient.http import MediaFileUpload
+
+import numpy as np
+#import argparse
+#import imutils
+from datetime import date
+
+import os
+from pdf2image import convert_from_path, convert_from_bytes
 from pyzbar.pyzbar import decode
 #curl -d "{\"Medidas\":[[1,2,3,4]]}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/predecir
 
